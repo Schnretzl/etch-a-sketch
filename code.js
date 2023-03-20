c08720dd53e7c9b5324013bd70d9d82c080df1d9
Wes Hendley <wrhendley@gmail.com>
2023-03-20 15:42:58 -0500

    Make divs squares, first code for color change

--- code.js
+++ code.js
@@ -2,15 +2,18 @@ const board = document.querySelector('#grid');
 let boardSize = 3;
 
 function createBoard(size){
+    let tileSize = parseFloat(500 / size);
     for(let row = 0; row < size; row++){
         for(let col = 0; col < size; col++){
             let square = document.createElement('div');
-            square.className += "square" + col + row;
+            square.setAttribute('id',"square" + row + col);
+            square.style.height = tileSize + 'px';
+            square.style.width = tileSize + 'px';
+            document.addEventListener('mouseover', () => square.style.backgroundColor = 'black');
             board.appendChild(square);
         }
     }
 }
 
-function changeColor(){}
 
 createBoard(boardSize);

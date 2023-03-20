715cf5a2b1ff7df42ac8c5e5aac0928d014aa1e3
Wes Hendley <wrhendley@gmail.com>
2023-03-20 11:49:10 -0500

    Add initial code to create divs for the board

--- code.js
+++ code.js
@@ -1,7 +1,16 @@
-const board = document.querySelector(#'grid');
-let boardSize = 16;
+const board = document.querySelector('#grid');
+let boardSize = 3;
 
-function createBoard(size){}
+function createBoard(size){
+    for(let row = 0; row < size; row++){
+        for(let col = 0; col < size; col++){
+            let square = document.createElement('div');
+            square.className += "square" + col + row;
+            board.appendChild(square);
+        }
+    }
+}
 
 function changeColor(){}
 
+createBoard(boardSize);
